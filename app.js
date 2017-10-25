e207e5bd98b0f473748b9fd4f1dc34f77853edc5
BrianPhiri <brianphiri.9523@gmail.com>
2017-10-25 15:37:53 +0300

    struct of the interfaces

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var contacts = require('./routes/contacts');
+var locks = require('./routes/locks')
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/contacts', contacts)
+app.use('/locks', locks)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
