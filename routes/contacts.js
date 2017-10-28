272023ad6d0f89fe475ea17382371cb1d1e66049
BrianPhiri <brianphiri.9523@gmail.com>
2017-10-29 00:29:37 +0300

    can add contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,26 @@
 var express = require('express')
+var request = require('request')
 
 var router = express.Router();
 
 router.get('/', function(req,res,next){
-    res.render('contacts');
+
+    request('https://cryptic-sands-49288.herokuapp.com/api/v1/contacts', function(error,request,body){
+        let obj = JSON.parse(body)
+        res.render('contacts', {data : obj});
+    })
+})
+
+router.post('/', function(req,res,next){
+    formData = {
+        email : req.body.email,
+        phoneNumber : req.body.phoneNumber,
+        role : req.body.role
+    }
+    request.post({url:'https://cryptic-sands-49288.herokuapp.com/api/v1/contacts', formData:formData}, function(err, httpResponse, body){
+        if(err) throw err
+        res.redirect('/contacts')
+    })
 })
 
 module.exports = router;
