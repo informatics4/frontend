10d162e348ff302d5ed033af5c175f06cb918429
BrianPhiri <brianphiri.9523@gmail.com>
2017-10-29 00:45:23 +0300

    added lock commands

--- routes/locks.js
+++ routes/locks.js
@@ -1,9 +1,22 @@
 var express = require('express')
+var request = require('request')
 
 var router = express.Router();
 
 router.get('/', function(req,res,next){
     res.render('locks');
+})
+
+router.post('/', function(req,res,next){
+    formData = {
+        room : req.body.room,
+        case : req.body.case,
+        action : req.body.action
+    }
+    request.post({url : 'https://cryptic-sands-49288.herokuapp.com/api/v1/commands', formData:formData}, function(err, httpResponse, body){
+        if(err) throw err
+        res.redirect('/locks')
+    })
 })
 
 module.exports = router;
