0997231b1a64e0a885f4aba268cf0140cd5f8806
BrianPhiri <brianphiri.9523@gmail.com>
2017-10-29 11:30:44 +0300

    added realtime lock status

--- routes/locks.js
+++ routes/locks.js
@@ -1,10 +1,28 @@
 var express = require('express')
 var request = require('request')
+const firebase = require('firebase')
 
 var router = express.Router();
 
+var config = {
+    apiKey: "AIzaSyBHHXfc9-TGQqGBoHYzT5pVe1lT13bBCLg",
+    authDomain: "fourthyear-d5634.firebaseapp.com",
+    databaseURL: "https://fourthyear-d5634.firebaseio.com",
+    projectId: "fourthyear-d5634",
+    storageBucket: "fourthyear-d5634.appspot.com",
+    messagingSenderId: "127657070211"
+  };
+
+  var app = firebase.initializeApp(config)
+  
+  var database  = firebase.database();
+
 router.get('/', function(req,res,next){
-    res.render('locks');
+    var lockStatsRef = database.ref('lock').once('value').then(function(snapshot){
+        let data = snapshot.val()
+        res.render('locks', {data : data})
+    })
+    // res.render('locks');
 })
 
 router.post('/', function(req,res,next){
@@ -18,5 +36,27 @@ router.post('/', function(req,res,next){
         res.redirect('/locks')
     })
 })
+
+router.post('/open', function(req,res,next){
+    formData = {
+        room : req.body.room,
+        action : req.body.action 
+    }
+    saveToFirebase(formData)
+    res.redirect('/locks')
+})
+
+router.post('/lock', function(req,res,next){
+    formData = {
+        room : req.body.room,
+        action : req.body.action 
+    }
+    saveToFirebase(formData)
+    res.redirect('/locks')
+})
+
+function saveToFirebase(message) {
+    database.ref('lock/'+message.room).set(message)
+}
 
 module.exports = router;
